303b28afbf1c6d5e7db11b88c787b6fce79a6bb6
yuenu <miraiku82@gmail.com>
2023-05-23 17:23:39 +0800

    fix: format date

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -6,10 +6,9 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import { Main } from '@/layout'
 import { allPosts } from 'contentlayer/generated'
-import matter from 'gray-matter'
+import { format } from 'date-fns'
 import Markdown from 'markdown-to-jsx'
 
-import { getPostMetadata } from '@/lib/post'
 import HeightLight from '@/components/HeightLight'
 
 async function getPageFromParams(props: PageProps) {
@@ -61,41 +60,41 @@ const PostPage = async (props: PageProps) => {
     return notFound()
   }
 
+  const parsedDate = format(new Date(post.date), 'yyyy/MM/dd')
+
   return (
-    <>
-      <Main className="post-details">
-        <HeightLight />
-        <h1 className="post-title mb-3 text-3xl font-semibold">{post.title}</h1>
-        <div className="mb-4">
-          <time dateTime={post.date}>{post.date}</time>
-        </div>
-        <p className="mb-10 italic opacity-70">{post.subtitle}</p>
-        <article className="post-content">
-          <Markdown
-            options={{
-              overrides: {
-                img: {
-                  component: Image,
-                  props: {
-                    width: 786,
-                    height: 500,
-                    className: 'w-full object-cover',
-                    priority: false,
-                  },
-                },
-                ul: {
-                  props: {
-                    className: 'space-y-2',
-                  },
+    <Main className="post-details">
+      <HeightLight />
+      <h1 className="post-title mb-3 text-3xl font-semibold">{post.title}</h1>
+      <div className="mb-4">
+        <time dateTime={parsedDate}>{parsedDate}</time>
+      </div>
+      <p className="mb-10 italic opacity-70">{post.subtitle}</p>
+      <article className="post-content">
+        <Markdown
+          options={{
+            overrides: {
+              img: {
+                component: Image,
+                props: {
+                  width: 786,
+                  height: 500,
+                  className: 'w-full object-cover',
+                  priority: false,
                 },
               },
-            }}
-          >
-            {post.body.raw}
-          </Markdown>
-        </article>
-      </Main>
-    </>
+              ul: {
+                props: {
+                  className: 'space-y-2',
+                },
+              },
+            },
+          }}
+        >
+          {post.body.raw}
+        </Markdown>
+      </article>
+    </Main>
   )
 }
 
--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
-import clsx from 'clsx'
 import type { Post } from 'contentlayer/generated'
+import { format } from 'date-fns'
+
+import { cn } from '@/lib/utils'
 
 export const PostPreview = (post: Post) => {
   return (
     <Link
       href={`/post/${post.slug}`}
-      className={clsx(
+      className={cn(
         'aLink group',
         'mb-4 block px-4 py-7',
         'space-y-4 rounded-lg border-2 border-transparent hover:border-orange-400'
@@ -14,7 +16,9 @@ export const PostPreview = (post: Post) => {
     >
       <div className="flex justify-between">
         <h2 className="text-xl group-hover:text-orange-400">{post.title}</h2>
-        <span className="whitespace-nowrap text-gray-400">{post.date}</span>
+        <span className="whitespace-nowrap text-gray-400">
+          {format(new Date(post.date), 'yyyy/MM/dd')}
+        </span>
       </div>
       <div className="italic opacity-70">{post.subtitle}</div>
     </Link>
