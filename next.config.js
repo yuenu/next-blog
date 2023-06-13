cca7e3de26e1e539375763bfb79d75a75808a6c2
yuenu <miraiku82@gmail.com>
2023-06-13 15:26:44 +0800

    feat: add headers settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
-// import { withContentlayer } from 'next-contentlayer'
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
   reactStrictMode: true,
 }
 
-// export default withContentlayer(nextConfig)
 module.exports = withContentlayer(nextConfig)
